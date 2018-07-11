089eab954d6a70d69367c980aef422d0dbc9d2a5
tdrana <30317977+tdrana@users.noreply.github.com>
2018-07-11 22:05:12 +0600

    js class 1

---
+++ main.js
@@ -0,0 +1,73 @@
+/*var a=10;
+var b=20;
+if(b>a)
+{
+	document.write(b);
+}
+var a=121;
+var b=123;
+document.write(a+b);
+console.log(a+b);
+
+var marks=90;
+if(marks<=100 && marks>=90)
+{
+	document.write("Marks is: A+");
+}else if(marks<90 && marks>80)
+{
+	document.write("Mark is:A");
+}else{
+	document.write("Grade is F");
+}
+
+switch(marks)
+{
+	case 90:
+	   document.write("A+");
+	   break;
+	default:
+	   document.write("F");
+	   break;   
+
+	}
+
+for(var i=0;i<=10;i++)
+{
+	document.write("hello");
+}
+
+var i=0;
+while(i<=10)
+{
+	document.write("Tanmoy");
+	i++;
+}
+
+do{
+document.write("Jhony");
+i++;
+}while(i<=10)
+
+var a=[10,20,30];
+
+for(i in a)
+{
+	document.write(a[i]);
+}
+
+function myFunction(var a, var b)
+{
+	document.write(a+b);
+
+}
+
+myFunction(10,20);
+
+*/
+function colorChange()
+{
+	var x=document.getElementById('myDiv');
+    x.style.backgroundColor="red";
+    x.innerHTML="HELLO THIS IS ME";
+}
+
